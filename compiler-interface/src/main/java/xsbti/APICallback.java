6283489aebe0039751c64612a8a81353c98596fb
Jamie Thompson <bishbashboshjt@gmail.com>
2020-02-28 14:57:55 +0100

    format java file

--- compiler-interface/src/main/java/xsbti/APICallback.java
+++ compiler-interface/src/main/java/xsbti/APICallback.java
@@ -24,44 +24,54 @@ public interface APICallback {
   }
 
   default void startSource(Path src) {}
+
   default void endSource() {}
 
   default void startClassLikeDef(int definitionType, String name) {}
+
   default void endClassLikeDef() {}
 
   default void startVal(String name) {}
+
   default void endVal() {}
 
   default void startDef(String name) {}
+
   default void endDef() {}
 
   default void startParamList(boolean isImplicit) {}
+
   default void endParamList() {}
 
   default void startProjection(String selected) {}
+
   default void endProjection() {}
 
   default void startConstant(Object constant) {}
+
   default void endConstant() {}
 
   default void startSingleton() {}
+
   default void endSingleton() {}
 
   default void startPath() {}
+
   default void endPath() {}
 
   default void id(String name) {}
-  default void thisId() {}
-  default void publicAPI() {}
-  default void modifiers(
-    boolean isAbstract,
-    boolean isOverride,
-    boolean isFinal,
-    boolean isSealed,
-    boolean isImplicit,
-    boolean isLazy,
-    boolean isMacro,
-    boolean isSuperAccessor
-  ) {}
 
+  default void thisId() {}
+
+  default void publicAPI() {}
+
+  default void modifiers(
+      boolean isAbstract,
+      boolean isOverride,
+      boolean isFinal,
+      boolean isSealed,
+      boolean isImplicit,
+      boolean isLazy,
+      boolean isMacro,
+      boolean isSuperAccessor) {}
 }
